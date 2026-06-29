10cacd1ad3e89375cb8181dbf7aaa32234a2649a
VuPL-dev <your-email@example.com>
2026-06-29 22:39:11 +0700

    Keep-only-fe-on-fe-branch

--- be/backend/src/routes/orderRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getUserOrders } from '../controllers/orderController';
-import { authenticateToken } from '../middlewares/authMiddleware';
-
-const router = Router();
-
-router.get('/', authenticateToken, getUserOrders);
-
-export default router;
--- be/backend/src/routes/paymentRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createOrder } from '../controllers/paymentController';
-import { authenticateToken } from '../middlewares/authMiddleware';
-
-const router = Router();
-
-router.post('/create-order', authenticateToken, createOrder);
-
-export default router;
