c12558289f7b70c8e230292bc2366f1ba2cad83f
VuPL-dev <your-email@example.com>
2026-06-29 22:38:54 +0700

    Keep-only-be-on-be-branch

--- fe/frontend_mobile/lib/utils/api_constants.dart
+++
@@ -1,4 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = String.fromEnvironment('API_URL', defaultValue: 'http://localhost:5000/api');
-  static const Duration requestTimeout = Duration(seconds: 15);
-}
